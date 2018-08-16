7377021ea7ec4cb278343ef2fac32f0ce919a4cc
Jon Robison <narfman0@gmail.com>
2018-08-15 23:54:09 -0400

    -Debug render some of world

--- foxtrot/models/world.py
+++ foxtrot/models/world.py
@@ -2,16 +2,33 @@
 import random
 
 from foxtrot.models.chunk import Chunk
+from foxtrot.models.npc import NPC
 
 
 class World:
-    def __init__(self, seed=None, size=5):
+    def __init__(self, seed=None, size=0):
+        """ Create world with given seed, will generate using system timestamp
+        if none given. Size expected in the range (0-10)
+        """
         random.seed(seed)
         planet_count = random.randint(2 + size // 2, 3 + size // 2)
         station_count = random.randint(2 * size - 2, 2 * size + 2)
         self.chunks = []
         for _ in range(planet_count):
-            self.chunks.append(Chunk(random, planet=True))
+            self.create_chunk(True)
         for _ in range(station_count):
-            self.chunks.append(Chunk(random))
-        # TODO ensure chunks have a position in the world
+            self.create_chunk(False)
+        self.create_player(self.chunks[0])
+
+    def create_chunk(self, planet, max_size=10000):
+        """ Create a chunk with a generated position
+        TODO ensure we dont collide with another chunk via bounding box
+        """
+        chunk = Chunk(random)
+        x = random.randint(chunk.size // 2, max_size - chunk.size // 2)
+        y = random.randint(chunk.size // 2, max_size - chunk.size // 2)
+        self.chunks.append((x, y, chunk))
+
+    def create_player(self, chunk_tuple):
+        chunk_x, chunk_y, chunk = chunk_tuple
+        self.player = NPC(x=chunk_x, y=chunk_y)
--- foxtrot/ui/screens/gameplay.py
+++ foxtrot/ui/screens/gameplay.py
@@ -3,6 +3,9 @@ import pyxel
 from foxtrot.models.world import World
 
 
+TILE_WIDTH = 8
+
+
 class GameplayScreen:
     def __init__(self, screen_manager):
         self.screen_manager = screen_manager
@@ -11,6 +14,40 @@ class GameplayScreen:
     def update(self):
         if pyxel.btnp(pyxel.KEY_BACKSPACE):
             self.screen_manager.pop()
+        if pyxel.btnp(pyxel.KEY_DOWN):
+            self.world.player.y += 1
+        if pyxel.btnp(pyxel.KEY_UP):
+            self.world.player.y -= 1
+        if pyxel.btnp(pyxel.KEY_LEFT):
+            self.world.player.x -= 1
+        if pyxel.btnp(pyxel.KEY_RIGHT):
+            self.world.player.x += 1
 
     def draw(self):
-        pyxel.text(pyxel.width / 2 - 16, 64, "gameplay", pyxel.frame_count % 16)
+        text = "x,y: %d,%d" % (self.world.player.x, self.world.player.y)
+        pyxel.text(pyxel.width / 2 - len(text) * 2, 4, text, pyxel.frame_count % 16)
+        pyxel.text(pyxel.width / 2 - 2, pyxel.height / 2 - 2, "o", 7)
+        index = 1
+        for x, y, chunk in self.world.chunks:
+            self.draw_chunk(x, y, chunk, index)
+            text = "x,y: %d,%d" % (x, y)
+            pyxel.text(pyxel.width / 2 - len(text) * 2, 4 + 8 * index, text, 8)
+            index += 1
+
+    def draw_chunk(self, x, y, chunk, index):
+        # TODO immediately skip chunks not nearby
+        if (
+            abs(self.world.player.x - x) - chunk.size // 2 > 64
+            or abs(self.world.player.y - y) - chunk.size // 2 > 64
+        ):
+            pass
+            # return
+        # draw chunk :)
+        for chunk_x, chunk_y, tile in chunk.tiles:
+            world_x = (x - chunk.size // 2 + chunk_x) - self.world.player.x
+            world_y = (y - chunk.size // 2 + chunk_y) - self.world.player.y
+            render_x = world_x * TILE_WIDTH + pyxel.width // 2
+            render_y = world_y * TILE_WIDTH + pyxel.height // 2
+            pyxel.text(
+                render_x - TILE_WIDTH // 2, render_y - TILE_WIDTH // 2, str(tile), 9
+            )
