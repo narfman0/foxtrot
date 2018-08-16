1eca21cebaa1cbdae0af8082269bac2c62a85bb2
Jon Robison <narfman0@gmail.com>
2018-08-16 14:42:00 -0400

    -Fix some render artifacts

--- foxtrot/ui/screens/gameplay.py
+++ foxtrot/ui/screens/gameplay.py
@@ -4,6 +4,7 @@ from foxtrot.models.world import World
 
 
 TILE_WIDTH = 8
+CHUNK_CULL_DISTANCE = 32
 
 
 class GameplayScreen:
@@ -15,9 +16,9 @@ class GameplayScreen:
         if pyxel.btnp(pyxel.KEY_BACKSPACE):
             self.screen_manager.pop()
         if pyxel.btnp(pyxel.KEY_DOWN):
-            self.world.player.y += 1
-        if pyxel.btnp(pyxel.KEY_UP):
             self.world.player.y -= 1
+        if pyxel.btnp(pyxel.KEY_UP):
+            self.world.player.y += 1
         if pyxel.btnp(pyxel.KEY_LEFT):
             self.world.player.x -= 1
         if pyxel.btnp(pyxel.KEY_RIGHT):
@@ -26,22 +27,23 @@ class GameplayScreen:
     def draw(self):
         text = "x,y: %d,%d" % (self.world.player.x, self.world.player.y)
         pyxel.text(pyxel.width / 2 - len(text) * 2, 4, text, pyxel.frame_count % 16)
-        pyxel.text(pyxel.width / 2 - 2, pyxel.height / 2 - 2, "o", 7)
+        self.draw_player()
         index = 1
         for x, y, chunk in self.world.chunks:
-            self.draw_chunk(x, y, chunk, index)
             text = "x,y: %d,%d" % (x, y)
             pyxel.text(pyxel.width / 2 - len(text) * 2, 4 + 8 * index, text, 8)
+            self.draw_chunk(x, y, chunk, index)
             index += 1
 
+    def draw_player(self):
+        pyxel.circ(pyxel.width / 2, pyxel.height / 2, TILE_WIDTH // 2, 7)
+
     def draw_chunk(self, x, y, chunk, index):
-        # TODO immediately skip chunks not nearby
         if (
-            abs(self.world.player.x - x) - chunk.size // 2 > 64
-            or abs(self.world.player.y - y) - chunk.size // 2 > 64
+            abs(self.world.player.x - x) - chunk.size // 2 > CHUNK_CULL_DISTANCE
+            or abs(self.world.player.y - y) - chunk.size // 2 > CHUNK_CULL_DISTANCE
         ):
-            pass
-            # return
+            return
         # draw chunk :)
         for chunk_x, chunk_y, tile in chunk.tiles:
             world_x = (x - chunk.size // 2 + chunk_x) - self.world.player.x
@@ -51,5 +53,5 @@ class GameplayScreen:
     def draw_tile(self, x, y, tile):
         """ Draw a tile given world coordinates x, y """
         render_x = x * TILE_WIDTH + pyxel.width // 2
-        render_y = y * TILE_WIDTH + pyxel.height // 2
-        pyxel.text(render_x - TILE_WIDTH // 2, render_y - TILE_WIDTH // 2, str(tile), 9)
+        render_y = -y * TILE_WIDTH + pyxel.height // 2
+        pyxel.text(render_x - 1, render_y - 2, str(tile), 9)
